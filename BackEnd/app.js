2baf9bc5ae7286232a22f098246db33fe4a09a9f
Imran Baali <ibaali@e1r12p10.1337.ma>
2022-07-25 00:15:03 +0100

    start listning in port 8080 instead of 3000 and fix cros bug

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const animeRoute = require('./controllers/controller.getImage')
+const express = require("express");
+const animeRoute = require("./controllers/controller.getImage");
+var cors = require('cors')
 
 const app = express();
-app.get('/api/avatar', animeRoute.getAnimeImage)
-app.get('/api/animelist', animeRoute.getAnimeList) //? return json
+app.use(cors())
+app.get("/api/avatar", animeRoute.getAnimeImage);
+app.get("/api/animelist", animeRoute.getAnimeList); //? return json
 app.use((req, res, next) => {
-    res.status(404).send("Page Not Found")
-})
+  res.status(404).send("Page Not Found");
+});
 
-app.listen(3000);
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
