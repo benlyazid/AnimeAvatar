5dadd5b1baee3e951fc1c18a088805ffa8ee3550
Khalid Benlyazid <kbenlyaz@e1r6p4.1337.ma>
2022-07-24 21:52:16 +0100

    clean the repos

--- tst.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-
-_response = requests.get('https://api.genderize.io/?name=peter')
-data = BeautifulSoup(_response.content, "html.parser")
-dataJson=json.loads(data.text)
-print(dataJson)
