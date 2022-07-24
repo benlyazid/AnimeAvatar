705a13a7a0c843d023e9fb591fffaf82e511d630
Imran Baali <ibaali@e1r12p10.1337.ma>
2022-07-25 00:43:50 +0100

    (Home page) add show avatar card and can change name, gender nad anime name

--- FrontEnd/src/components/Home/TopSection/LeftSection.tsx
+++ FrontEnd/src/components/Home/TopSection/LeftSection.tsx
@@ -5,7 +5,7 @@ const LeftSection: React.FC = () => {
 		<>
 			<h6 className="text-40-bold">Anime list avatars</h6>
 			<h1 className="grey-clr text-18-regular main-clr">
-				is an anime characters avatar library for designers and
+				is an anime characters avatar library for normal users, designers and
 				developers. You can choose lovely anime characters characters.
 			</h1>
 			<h2 className="grey-clr mt-3 text-18-regular main-clr">
@@ -13,7 +13,7 @@ const LeftSection: React.FC = () => {
 				can use right away!
 			</h2>
 			<div className="mt-4 d-flex align-items-center">
-				<button className="main-btn">Create Avatar</button>
+				<button className="main-btn">Create Your Avatar</button>
 				<p className="mb-0">
 					<span className="ml-3 grey-clr">or</span>
 					<Link
