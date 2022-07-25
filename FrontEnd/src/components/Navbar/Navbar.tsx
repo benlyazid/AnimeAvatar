2286a9aa3ffb8e2e0f6670895bbb4fa36c9ea35c
kirwa <kirwa@vermout.local>
2022-07-25 15:56:20 +0100

    add docs page

--- FrontEnd/src/components/Navbar/Navbar.tsx
+++ FrontEnd/src/components/Navbar/Navbar.tsx
@@ -9,17 +9,17 @@ const Navbar: React.FC = () => {
 		<div className="navbar-container">
 			<div className="container py-4">
 				<div className="row m-0 align-items-end justify-content-between">
-					<div className="row align-items-end">
+					<Link to="/" className="row align-items-end link">
 						<Logo />
 						<span className="white-clr h5 font-weight-600 mb-0 d-none d-lg-inline">Anime list avatras</span>
 						<span className="white-clr h5 font-weight-600 mb-0 d-inline d-lg-none">ALV</span>
-					</div>
+					</Link>
 					<div className="links-container">
 						<Link  className="link-item icon-animate" to="/docs">
 							<DocsIcon className="icon" fill="white" height={18.8} width={18.8} />
 							Docs
 						</Link>
-						<OutsideLink className="link-item github-corner" link="/">
+						<OutsideLink className="link-item github-corner" link="https://github.com/benlyazid/AnimeAvatar">
 							<>
 
 								<GithubIcon />
---
+++ FrontEnd/src/pages/Docs.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar/Navbar";
+import DocsBody from "../components/Docs/DocsBody/DocsBody";
+import Footer from "../components/Footer/Footer";
+
+const Docs: React.FC = () => {
+  return (
+	<>
+		<Navbar />
+		<DocsBody />
+		<Footer />
+	</>
+  );
+}
+
+export default Docs;
