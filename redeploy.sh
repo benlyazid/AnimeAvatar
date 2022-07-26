b4d09015414bb9e2cb6ce91cc7699d5cdfd7bbc2
kirwa <kirwa@vermout.local>
2022-07-26 09:43:19 +0100

    (deployement) change redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 sudo docker-compose down
-# sudo docker volume rm ocp_booking_caddy_config
-# sudo docker volume rm ocp_booking_caddy_data
-sudo docker volume rm ocp_booking_static-content
+# sudo docker volume rm animeavatar_caddy_config
+# sudo docker volume rm animeavatar_caddy_data
+sudo docker volume rm animeavatar_static-content
 git pull
 sudo docker-compose up --build -d
