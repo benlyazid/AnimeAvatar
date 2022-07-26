ac80c104961a4fd6deaa6674c78c899e3b8d90ac
Imran Baali <imran.baali19@gmail.com>
2022-07-26 18:23:56 +0100

    (deployement) start using github actionsto redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -1,17 +1,6 @@
-
-while true
-do
-	git pull | grep "Already up to date."
-	if [ $? -eq 0 ]
-	then
-		echo "No changes detected. Exiting."
-	else
-		sudo docker-compose down
-		# sudo docker volume rm animeavatar_caddy_config
-		# sudo docker volume rm animeavatar_caddy_data
-		sudo docker volume rm animeavatar_static-content
-		git pull
-		sudo docker-compose up --build -d
-	fi
-	sleep 60
-done
+sudo docker-compose down
+# sudo docker volume rm animeavatar_caddy_config
+# sudo docker volume rm animeavatar_caddy_data
+sudo docker volume rm animeavatar_static-content
+git pull
+sudo docker-compose up --build -d
