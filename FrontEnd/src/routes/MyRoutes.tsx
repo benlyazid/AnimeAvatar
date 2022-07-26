bd645cbde073aa2a2cdf1eecbd888471cf309819
kirwa <kirwa@vermout.local>
2022-07-26 12:00:27 +0100

    (frontend) add lazy loading in frontend

--- FrontEnd/src/routes/MyRoutes.tsx
+++ FrontEnd/src/routes/MyRoutes.tsx
@@ -1,20 +1,24 @@
+import { lazy, Suspense } from "react";
 import {
 	BrowserRouter as Router,
 	Navigate,
 	Route,
 	Routes
 } from "react-router-dom";
-import Docs from "../pages/Docs";
 import Home from "../pages/Home";
+
+const Docs = lazy(() => import("../pages/Docs"));
 
 const MyRoutes: React.FC = () => {
 	return (
 			<Router>
-				<Routes>
-					<Route path="/docs" element={<Docs />} />
-					<Route path="/" element={<Home />} />
-					<Route path="*" element={<Navigate to="/" replace />} />
-				</Routes>
+				<Suspense fallback={<div className="second-clr" style={{ textAlign: "center", marginTop: "1rem" }}>Loading...</div>}>
+					<Routes>
+						<Route path="/docs" element={<Docs />} />
+						<Route path="/" element={<Home />} />
+						<Route path="*" element={<Navigate to="/" replace />} />
+					</Routes>
+				</Suspense>
 			</Router>
 	);
 };
