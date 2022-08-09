9f0d801aeed7c7f699c02d7ae6324647e1f15c8b
Khalid Benlyazid <kbenlyaz@e1r6p2.1337.ma>
2022-08-09 20:12:16 +0100

    debug new solution

--- BackEnd/middlewares/middleware.updateStatistiques.js
+++ BackEnd/middlewares/middleware.updateStatistiques.js
@@ -1,13 +1,8 @@
 const updateStatistiques = (req, res, next) => {
     global.statistiques.numberOfRequests += 1
-    console.log("new country is " + req.country)
-    console.log("all countries are " + global.countries)
-    console.log("serch for  " + global.countries.indexOf(req.country))
     if(global.countries.indexOf(req.country) == -1){
         global.countries.push(req.country)
         global.statistiques.countries += 1
-        console.log("append  " + (req.country))
-        console.log("list after append  " + global.countries)
     }
     next()
 }
